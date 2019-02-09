7e9fb1277ad9d4d12635bd1cccf557083c90b2ac
anthony <delgado.anthony@gmail.com>
2019-02-09 11:16:56 +0100

    add test anonymous access to admin

---
+++ tests/Controller/AdminControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anthony
+ * Date: 09/02/19
+ * Time: 10:59
+ */
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdminControllerTest extends WebTestCase
+{
+    public function testfailed()
+    {
+        $client = $this->createClient();
+
+        $client->request('GET', '/admin/import');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    protected static function getKernelClass()
+    {
+        return \App\Kernel::class;
+    }
+}
